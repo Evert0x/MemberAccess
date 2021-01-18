77bb7eed14e43adba8d1a5befe72ac81a3f9591a
Evert Kors <evert@hatching.io>
2021-01-18 21:27:45 +0100

    Change script

--- scripts/dep.bash
+++ scripts/dep.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJ_DIR="$( dirname " $DIR ")"
 
+touch .tmp
 if [ "$1" == "$(cat .tmp)" ] ;then
     :
 else
     echo "using" $1
 
     rm -rf $2
-    git clone $1 $2
+    git clone $1 $PROJ_DIR/$2
 
     echo $1 $2 > .tmp
 fi
